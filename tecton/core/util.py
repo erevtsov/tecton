ef7c576381494e6d099d30eeb2497d547641aa84
Yevgen Revtsov <erevtsov@gmail.com>
2025-04-28 21:12:15 -0400

    feat: implement TableSet/Config to harden table naming

--- tecton/core/util.py
+++ tecton/core/util.py
@@ -1,3 +1,6 @@
+import yaml
+
+
 def load_reference(
     module: str,
     object: str = None,
@@ -34,3 +37,61 @@ def load_reference(
         return func
     else:
         return obj
+
+
+class TableSet:
+    """
+    A nested structure of TableConfig objects
+    """
+
+    def __init__(self, data: dict):
+        for key, value in data.items():
+            if isinstance(value, dict):
+                # If this child has a 'path', it's a TableConfig (leaf)
+                if 'path' in value:
+                    setattr(self, key, TableConfig(value))
+                else:
+                    setattr(self, key, TableSet(value))
+            else:
+                setattr(self, key, value)
+
+    def __str__(self):
+        def tree_view(obj, indent=''):
+            result = []
+            for key, value in obj.__dict__.items():
+                if isinstance(value, TableSet):
+                    result.append(f'{indent}{key}')
+                    result.append(tree_view(value, indent + '  '))
+                elif isinstance(value, TableConfig):
+                    result.append(f'{indent}{key}')
+                else:
+                    result.append(f'{indent}{key}: {value}')
+            return '\n'.join(result)
+
+        return tree_view(self)
+
+    __repr__ = __str__
+
+
+class TableConfig:
+    """
+    A configuration object for a table
+    """
+
+    def __init__(self, data: dict):
+        for key, value in data.items():
+            setattr(self, key, value)
+
+    def __str__(self):
+        return str(getattr(self, 'path', ''))
+
+    __repr__ = __str__
+
+
+def load_yaml_tables(file_path: str) -> TableSet:
+    """
+    Load a YAML file containing table configurations and return a TableSet object.
+    """
+    with open(file_path) as f:
+        data = yaml.safe_load(f)
+    return TableSet(data)
